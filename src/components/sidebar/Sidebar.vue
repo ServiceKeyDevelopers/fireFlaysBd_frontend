3674c44437af27b30db90a2c5b437d3f3244bb65
ALBAB404 <imalbab4@gmail.com>
2024-06-13 15:34:09 +0600

    header bug fixes

--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -2,7 +2,7 @@
 <script setup>
 import { MiddleHeader, TopHeader } from "@/components";
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 // slider get data 
 import { storeToRefs } from 'pinia';
@@ -17,6 +17,7 @@ const {categories} = storeToRefs(category)
 const activeIndex = ref(null);
 
 const route = useRoute();
+const router = useRouter();
 const isActiveRoute = (routeName) =>  {
       return route.name === routeName;
     }
@@ -33,6 +34,10 @@ const toggleDropdown = (index, category) => {
     }
  }
 
+ const toggleMenu = () => {
+  document.body.classList.toggle("sidebar-opened");
+};
+
 
 onMounted(() => {
     category.getData();
@@ -47,12 +52,12 @@ onMounted(() => {
 
                     <nav class="side-nav">
                         <ul class="menu menu-vertical main-vertical sf-arrows d-block pb-0 no-superfish">
-                            <li :class="{ 'active': isActiveRoute('ShopPage') }"><router-link :to="{name: 'ShopPage'}">Shop</router-link></li>
-                            <li :class="{ 'active': isActiveRoute('CheckoutPage') }"><router-link :to="{name: 'CheckoutPage'}">Checkout Page</router-link></li>
-                            <li :class="{ 'active': isActiveRoute('WishlistPage') }"><router-link :to="{name: 'WishlistPage'}">Wishlist</router-link></li>
-                            <li :class="{ 'active': isActiveRoute('AboutPage') }"><router-link :to="{name: 'AboutPage'}">About Us</router-link></li>
-                            <li :class="{ 'active': isActiveRoute('ContactPage') }"><router-link :to="{name: 'ContactPage'}">Contact Us</router-link></li>
-                            <li :class="{ 'active': isActiveRoute('LoginPage') }"><router-link :to="{name: 'LoginPage'}">Login & Register</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('ShopPage') }"><router-link :to="{name: 'ShopPage'}" @click="toggleMenu()">Shop</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('CheckoutPage') }"><router-link :to="{name: 'CheckoutPage'}" @click="toggleMenu()">Checkout Page</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('WishlistPage') }"><router-link :to="{name: 'WishlistPage'}" @click="toggleMenu()">Wishlist</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('AboutPage') }"><router-link :to="{name: 'AboutPage'}" @click="toggleMenu()">About Us</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('ContactPage') }"><router-link :to="{name: 'ContactPage'}" @click="toggleMenu()">Contact Us</router-link></li>
+                            <li :class="{ 'active': isActiveRoute('LoginPage') }"><router-link :to="{name: 'LoginPage'}" @click="toggleMenu()">Login & Register</router-link></li>
                         </ul>
                     </nav>
 
@@ -79,5 +84,8 @@ onMounted(() => {
 <style>
 .category-sidebar-active{
     color: #00b37c;
+}
+.sidebar-home {
+    z-index: 1 !important;
 }
 </style>
